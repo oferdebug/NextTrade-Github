0c44bce4fdef739dd49e9f8503f6e3155edfc801
oferdebug <ofercohen404@gmail.com>
2025-12-23 11:15:39 +0200

    added auth ui components

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -2,6 +2,9 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
 import {Button} from "@/components/ui/button";
 import InputField from "@/components/forms/inputField";
+import SelectField from "@/components/forms/SelectField";
+import {INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS} from "@/lib/constants";
+import CountrySelectField from "@/components/forms/countrySelectField";
 
 
 
@@ -50,6 +53,84 @@ const {
                     error={errors.fullName}
                     validation={{required:'Full Name Is Required',minLength:2}}
                 />
+                <InputField
+                    name={'email'}
+                    label={'Email'}
+                    placeholder={'contact@NextTrade.com'}
+                    register={register}
+                    error={errors.email}
+                    validation={{required:'Full Email Is Required',minLength:2}}
+                />
+                <InputField
+                    name={'Password'}
+                    label={'Password'}
+                    placeholder={'Enter Is A Strong Password'}
+                    register={register}
+                    type={'password'}
+                    error={errors.fullName}
+                    validation={{required:'Password Is Required',minLength:2}}
+                />
+                {/*<InputField*/}
+                {/*    name={'fullName'}*/}
+                {/*    label={'Full Name'}*/}
+                {/*    placeholder={'Sarah Mitchell'}*/}
+                {/*    register={register}*/}
+                {/*    error={errors.fullName}*/}
+                {/*    validation={{required:'Full Name Is Required',minLength:2}}*/}
+                {/*/>*/}
+                {/* County */}
+                <CountrySelectField />
+
+                <SelectField
+                name='invetmentGoals'
+                label={'Investment Goals'}
+                placeholder={'Select Your Investment Goals'}
+                options={INVESTMENT_GOALS}
+                control={control}
+                error={errors.investmentGoals}
+                required
+                />
+                <SelectField
+                    name='preferredIndustry'
+                    label={'Preferred Industry'}
+                    placeholder={'Select Your Preferred Industry'}
+                    options={PREFERRED_INDUSTRIES}
+                    control={control}
+                    error={errors.riskTolerance}
+                    required
+                />
+                <SelectField
+                    name='riskTolerance'
+                    label={'Risk Tolerance'}
+                    placeholder={'Select Your Risk Level'}
+                    options={RISK_TOLERANCE_OPTIONS}
+                    control={control}
+                    error={errors.riskTolerance}
+                    required
+                />
+                {/*<InputField*/}
+                {/*    name={'fullName'}*/}
+                {/*    label={'Full Name'}*/}
+                {/*    placeholder={'Sarah Mitchell'}*/}
+                {/*    register={register}*/}
+                {/*    error={errors.fullName}*/}
+                {/*    validation={{required:'Full Name Is Required',minLength:2}}*/}
+                {/*/>*/}
+                {/*<InputField*/}
+                {/*    name={'fullName'}*/}
+                {/*    label={'Full Name'}*/}
+                {/*    placeholder={'Sarah Mitchell'}*/}
+                {/*    register={register}*/}
+                {/*    error={errors.fullName}*/}
+                {/*    validation={{required:'Full Name Is Required',minLength:2}}*/}
+                {/*/>*/}
+
+
+
+
+
+
+
                 <Button type={'submit'} disabled={isSubmitting} className={'green-btn w-full mt-5'}>
                     {isSubmitting?'Creating Account':'Start Your Investing Journey Today'}
                 </Button>
